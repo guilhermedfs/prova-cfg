a96152444adc2561c36cd1ba9e867a17dc1ce651
Guilherme Daniel Fernandes da Silva <guilhermedfs11@gmail.com>
2022-03-23 19:10:17 -0300

    Função gerador feita.

--- gerador.py
+++ gerador.py
@@ -28,27 +28,56 @@ Abaixo seguem alguns exemplos de strings aleatórias válidas geradas por esta g
     fizz(fizz(fizz, buzz(10, 42, 1, 100, buzz(1, buzz))))
 """
 import random
+from unittest import result
 
+strings = {
+    "f1": "fizz", 
+    "f2": "buzz"
+}
+
+symbols = {
+    "open": "(",
+    "close": ")",
+    "virgule": ",",
+    "whitespace": " "
+}
+
+def separator():
+    src = str(random.randint(1, 100)) + symbols["virgule"] + symbols["whitespace"]
+    if random.random() < 0.5:
+        src += separator()
+    return src
 
 def random_example() -> str:
-    return "..."
+    return fizzFunc()
 
+def fizzFunc():
+    value = "" # Initialize string
+    result = range(0, 20)
+    randResult = random.choice(result)
+    
+    if(randResult %2 == 0):
+        value += strings["f1"]
+    else:
+        value += strings["f1"] + symbols["open"] + fizzFunc()
+        if random.random() < 0.5:
+            value += symbols["virgule"] + symbols["whitespace"] + buzzFunc()
+        value += symbols["close"]
+    return value
 
-def fizzbuzz():
-    return "..."
-
-
-def fizz():
-    return "..."
-
-
-def buzz():
-    return "..."
-
-
-def NUMBER():
-    return str(random.randint(1, 100))
-
+def buzzFunc():
+    value = "" # Initialize string
+    result = range(0, 20)
+    randResult = random.choice(result)
+    
+    if(randResult % 2 == 0):
+        value += strings["f2"]
+    else:
+        value += strings["f2"] + symbols["open"]
+        value += separator()
+        value += buzzFunc()
+        value += symbols["close"]
+    return value
 
 if __name__ == "__main__":
     for i in range(1, 11):
